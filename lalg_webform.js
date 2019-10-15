7e6efec2f6e8c266cb4af90431c853139ef10721
lalgwebdev <webtech@lalg.org.uk>
2019-10-15 11:25:24 +0300

    Disable routine which sets Postcode Clone.  Not needed

--- lalg_webform.js
+++ lalg_webform.js
@@ -114,18 +114,19 @@ $(document).ready(function(){
 	
 //****************************************************************
 // Copy Postcode into clone fields on Contact when Next Page clicked
-	$(".form-actions input.webform-next").click(function(){
-		var pc = $("input.lalg-wf-postcode").val();
-//		console.log(pc);
-		// Always do Member 1
-		$("fieldset.lalg-wf-fs-member1 input.lalg-wf-postcode-clone").val(pc);
-		// Check each Additional Member to see if it exists
-		$("fieldset.lalg-wf-fs-additional-member input.form-text").each(function(index, el) {
-			if ($(this).val()) {
-				$(this).parent().parent().find("input.lalg-wf-postcode-clone").val(pc);
-			};	
-		});	
-	});
+// ************** Commented out - Not needed.  Pending deletion  *************
+	// $(".form-actions input.webform-next").click(function(){
+		// var pc = $("input.lalg-wf-postcode").val();
+// //		console.log(pc);
+		// // Always do Member 1
+		// $("fieldset.lalg-wf-fs-member1 input.lalg-wf-postcode-clone").val(pc);
+		// // Check each Additional Member to see if it exists
+		// $("fieldset.lalg-wf-fs-additional-member input.form-text").each(function(index, el) {
+			// if ($(this).val()) {
+				// $(this).parent().parent().find("input.lalg-wf-postcode-clone").val(pc);
+			// };	
+		// });	
+	// }); 
 	
 });				// End Document Ready
 })(jQuery);		// ******************* Close the $ reversion	
