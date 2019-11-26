156a4e9f740ecc2ebc1db5ba54597c5d19067a2b
lalgwebdev <webtech@lalg.org.uk>
2019-11-26 14:23:36 +0200

    Changes for changing Membership process flag to Tags

--- lalg_webform.js
+++ lalg_webform.js
@@ -34,67 +34,34 @@ $(document).ready(function(){
 //*********************** DEFAULTS ****************************************
 // Actions required on first load of first page
 
-// Actions for End-User Form
-// For the Send Membership Documents field
-	$('div.form-radios.lalg-wf-user-card-reqd').each(function() {
-		if ($('div.form-radios.lalg-wf-membership-type').is(':visible')){
-			// If Membership Renewal is shown, and mandatory, set the Printed Card Flags and hide them
-			$('div.lalg-wf-email-mship-receipt.form-radios div:nth-of-type(1) input').click();
-			$('div.lalg-wf-user-card-reqd.form-radios div:nth-of-type(1) input').click();
-			$('div.lalg-wf-user-card-reqd-wrapper').hide();
-		}
-		else {
-			// If no Membership renewal option shown then change label.
-			$(this).parent().find('> label').text('Replacement Membership Card');	
-		}	
-	});
-
-// Default Membership Type Type Required to None on first load
+// Default Membership Type Type Required to None on first load (Admin Form - User form has Radios)
 //	console.log(document.referrer);
 	if (!document.referrer.includes('admindetails')) { 
 //		console.log('Setting Type Required = None');
 		$("select.lalg-wf-membership-type :nth-child(1)").prop('selected', true);
 	}
+// Hide 'Membership Requested' Tag, and Label of Print Card Tag
+	$("div.lalg-wf-tag div:nth-of-type(1)").hide();
+	$("div.lalg-wf-tag div:nth-of-type(2) label").hide();
 		
 //*************************** DOCUMENT TYPE & DELIVERY ************************************
 // Actions when Membership Type changed
-	$(".lalg-wf-membership-type").change(function(){
+	$("select.lalg-wf-membership-type").change(function(){
 //		console.log($(this).val());
 	// Set Email Newsletter Option if Plain Membership selected
 		if($(this).val() == 7) {
 			$("input.lalg-wf-emailoptions[data-civicrm-field-key$='contact_1_cg4_custom_9']" ).prop('checked', true);
 		}
-		
-		// Set Membership Card Flags when Membership selected or deselected
-		if ($(this).val()) {
-			// Some Membership selected - Set flags 
-			$('div.lalg-wf-email-mship-receipt.form-radios div:nth-of-type(1) input').click();
-			$('div.lalg-wf-card-reqd.form-radios div:nth-of-type(1) input').click();	// Admin Form
+	// Set 'Membership Requested' Tag if any Membership selected.
+		if($(this).val()) {
+//			console.log('Set Flag');
+			$("div.lalg-wf-tag div:nth-of-type(1) input").prop('checked', true);
 		}
 		else {
-			// No Membership selected - Clear flags
-			$('div.lalg-wf-email-mship-receipt.form-radios div:nth-of-type(2) input').click();
-			$('div.lalg-wf-card-reqd.form-radios div:nth-of-type(2) input').click();	// Admin Form
+			$("div.lalg-wf-tag div:nth-of-type(1) input").prop('checked', false);	
 		}
 	});
-	
-// Replacement Card Actions
-	// Printed card request changed
-	$('div.lalg-wf-user-card-reqd.form-radios input').change(function(){
-//		console.log($(this).val());
-		// Check Replacement Card state
-		if (!$('div.form-radios.lalg-wf-membership-type').is(':visible')){
-			// Card requested - set email flag
-			if($(this).val() == 1) {
-				$('div.lalg-wf-email-mship-receipt.form-radios div:nth-of-type(1) input').click();
-			}
-			// Clear email flag
-			else {
-				$('div.lalg-wf-email-mship-receipt.form-radios div:nth-of-type(2) input').click();
-			}
-		}
-	});		
-	
+
 
 //*********************** OTHERS *****************************************
 // Default Household Name for new Contact	
