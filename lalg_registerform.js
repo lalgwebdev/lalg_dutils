1ab908a2982509727383e09054c051c2c275d188
lalgwebdev <webtech@lalg.org.uk>
2020-04-22 13:21:09 +0300

    More JS Validation on Register Form

--- lalg_registerform.js
+++ lalg_registerform.js
@@ -8,6 +8,10 @@
 $(document).ready(function(){
 //console.log("Register form Loaded");
 
+// **********************  One-off functions on Page load  **************
+	$('div#helprow-first_name div.description').css({'font-weight':'bold'});
+
+
 // *********************  Capitalise Postcode field  ********************
 // *********************  and check valid format     ********************
 	$("input#postal_code-Primary").change(function(){
@@ -20,11 +24,40 @@ $(document).ready(function(){
 			$('div#edit-actions input#edit-submit').prop('disabled', false);
 		}
 		else {
-			$('div#helprow-postal_code-Primary div.description').text('The Postcode format is incorrect, please correct it');
+			$('div#helprow-postal_code-Primary div.description').text('The Postcode format is incorrect, please correct it.').css({'font-weight':'bold', 'color':'red'});
 			$('div#edit-actions input#edit-submit').prop('disabled', true);
 		}
 	});
-
+	
+// **********************  Check Names have Upper and Lower Case  **************************
+	$("input#first_name").change(function(){
+		uc = $(this).val().match(/[A-Z]/);
+		lc = $(this).val().match(/[a-z]/);
+//		console.log(uc);
+//		console.log(uc != null && lc != null);
+		if (uc != null && lc != null) {
+			$('div#helprow-postal_code-Primary div.description').text('');
+			$('div#edit-actions input#edit-submit').prop('disabled', false);
+		}
+		else {
+			$('div#helprow-postal_code-Primary div.description').text('Names should contain both upper and lower case letters.').css({'font-weight':'bold', 'color':'red'});
+			$('div#edit-actions input#edit-submit').prop('disabled', true);
+		}		
+	});
+	
+	$("input#last_name").change(function(){
+		uc = $(this).val().match(/[A-Z]/);
+		lc = $(this).val().match(/[a-z]/);
+		if (uc != null && lc != null) {
+			$('div#helprow-postal_code-Primary div.description').text('');
+			$('div#edit-actions input#edit-submit').prop('disabled', false);
+		}
+		else {
+			$('div#helprow-postal_code-Primary div.description').text('Names should contain both upper and lower case letters.').css({'font-weight':'bold', 'color':'red'});
+			$('div#edit-actions input#edit-submit').prop('disabled', true);
+		}		
+	});
+	
 
 	
 });				// End Document Ready
